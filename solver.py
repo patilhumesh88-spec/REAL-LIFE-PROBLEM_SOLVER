81d3bad2365a55c02c74006f4ab349addae7b96e
_humesh7 <patilhumesh88@gmail.com>
2026-03-20 18:32:03 +0530

    THIS IS THE END!!!

---
+++ solver.py
@@ -0,0 +1,8 @@
+A=("Then Why Worry!!!")
+problem = input("Do You Have Problem In Your Life? (yes/no):").lower()
+if problem == "no":
+    print(A)
+else:
+    SOL=input("Can You Do Something About It? (yes/no):").lower()
+    print(A)
+ 
